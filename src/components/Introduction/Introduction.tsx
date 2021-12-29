e4751c4ce883fbc8cd236f9d2335210dc2b750bb
Frederic Bahr <fredericbahr1@gmail.com>
2021-12-29 20:15:14 +0100

    add introduction

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -1,16 +1,98 @@
-import React from 'react'
-import styled from "styled-components"
+import { lighten, Slide, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Typewriter from "typewriter-effect";
+import {
+  introductionDelay,
+  typewriterDeleteDelay,
+  typewriterInitialDelay,
+} from "../../utils/constants";
+import { Character } from "../Character/Character";
+import { StyledLink } from "../util-components/StyledLink";
+import "./Introduction.css";
 
-const Wrapper = styled.div`
-    color: ${props => props.theme.colors.whitefontcolor};
-    width: 100vw;
-    height: 100vh;
-`
+type TypographyProps = {
+  component: React.ElementType;
+};
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  width: 85vw;
+  /** 50px navbar and 2*5vh margin */
+  min-height: calc(100vh - 50px - 10vh);
+  margin: 5vh auto;
+`;
+
+const TextWrapper = styled.div``;
+
+const StyledBeforeTypwriter = styled(Typography)<TypographyProps>`
+  color: ${(props) => lighten(props.theme.colors.mainblack, 0.4)};
+  font-size: 1.2rem;
+  margin-bottom: 3vh;
+`;
+
+const StyledSubtitle = styled(Typography)`
+  color: ${(props) => lighten(props.theme.colors.mainblack, 0.8)};
+  font-size: 2rem;
+`;
+
+const StyledText = styled(Typography)`
+  font-size: 1.2rem;
+  margin-top: 5vh;
+  color: ${(props) => lighten(props.theme.colors.mainblack, 0.8)};
+  width: 75%;
+`;
 
 export const Introduction = () => {
-    return (
-        <Wrapper>
-            <p>TESt</p>
-        </Wrapper>
-    )
-}
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    //const timeout = setTimeout(() => setIsMounted(true), introductionDelay);
+    const timeout = setTimeout(() => setIsMounted(true), 0);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <Slide direction="right" in={isMounted} timeout={400}>
+      <Grid>
+        <TextWrapper>
+          <StyledBeforeTypwriter variant="h6" component={"p"}>
+            Hi, I am{" "}
+          </StyledBeforeTypwriter>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .pauseFor(typewriterInitialDelay)
+                .typeString("Frederic Bahr")
+                .pauseFor(typewriterDeleteDelay)
+                .deleteAll()
+                .typeString("a Media Informatics student.")
+                .pauseFor(typewriterDeleteDelay)
+                .deleteAll()
+                .typeString("a Frontend Web Developer.")
+                .pauseFor(typewriterDeleteDelay)
+                .deleteAll()
+                .typeString("Frederic Bahr")
+                .start();
+            }}
+          />
+          <StyledSubtitle>
+            I develop occasionally well assets for the web.
+          </StyledSubtitle>
+          <StyledText>
+            I am a Software Engineer spezializing in building digital experience
+            from the frontend perspective. Currently studying Media Informatics
+            (Master) at{" "}
+            <StyledLink url="https://www.htwk-leipzig.de/startseite/">
+              HTWK
+            </StyledLink>{" "}
+            and developing UI-Components at{" "}
+            <StyledLink url="https://www.ipoque.com/">ipoque GmbH</StyledLink>.
+          </StyledText>
+        </TextWrapper>
+        <Character />
+      </Grid>
+    </Slide>
+  );
+};
---
+++ src/components/util-components/StyledLink.tsx
@@ -0,0 +1,38 @@
+import React, { FunctionComponent } from "react";
+import styled from "styled-components";
+import { transition } from "../../utils/constants";
+
+interface IProps {
+  url: string;
+}
+
+const Link = styled.a`
+    text-decoration: none;
+    color: ${props => props.theme.colors.mainred};
+    transition: ${transition};
+    position: relative;
+    display: inline-block;
+
+    &:hover {
+      &::after {
+        width: 100%;
+      }
+    }
+
+    &::after {
+      content: "";
+      display: block;
+      width: 0;
+      height: 1.5px;
+      position: relative;
+      background-color: ${props => props.theme.colors.mainred};
+      opacity: 0.5;
+      transition: ${transition};
+      bottom: 0.2rem;
+    }
+    
+`
+
+export const StyledLink: FunctionComponent<IProps> = ({ url, children }) => {
+  return <Link href={url} target={"_blank"}>{children}</Link>;
+};
