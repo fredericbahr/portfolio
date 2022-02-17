7dd6ab2735a33c6bbe78a4c8e49578ac4c1e02de
fbahr <frederic.bahr@stud.htwk-leipzig.de>
2022-02-17 16:19:19 +0100

    adjust footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,25 +1,32 @@
 import { Typography } from "@mui/material";
 import React from "react";
 import styled from "styled-components";
+import { StyledLink } from "../util-components/StyledLink";
 
 const Wrapper = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   padding-bottom: 5vh;
 `;
 
 const StyledTypography = styled(Typography)`
   font-size: 0.8rem;
-
-  &:hover {
-    color: ${(props) => props.theme.colors.mainred};
-  }
+  display: block;
 `;
+
+const StyledInspiration = styled(Typography)`
+  font-size: 0.65rem;
+  margin-top: 0.3rem;
+`
 
 export const Footer = () => {
   return (
     <Wrapper>
       <StyledTypography>Designed & Build by Frederic Bahr</StyledTypography>
+      <StyledInspiration>
+        Inspired by <StyledLink url="https://brittanychiang.com/">Brittany Chiang</StyledLink>
+      </StyledInspiration>
     </Wrapper>
   );
 };
