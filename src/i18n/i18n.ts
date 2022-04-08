7824e0f18814ac3c6775911ed46a9db90fda3fba
Frederic Bahr <fredericbahr1@gmail.com>
2022-04-08 21:41:02 +0200

    add language detection

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,4 +1,5 @@
 import i18n from "i18next";
+import LanguageDetector, { DetectorOptions } from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import translation_de from "./locales/de/translation.json";
 import translation_en from "./locales/en/translation.json";
@@ -15,11 +16,17 @@ const resources = {
   },
 };
 
+const detectionOptions: DetectorOptions = {
+  order: ["querystring", "navigator"],
+  lookupQuerystring: "lng",
+};
+
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
   .init({
     resources,
-    lng: "en",
+    detection: detectionOptions,
     fallbackLng: "en",
     interpolation: {
       escapeValue: false, // react already safes from xss
